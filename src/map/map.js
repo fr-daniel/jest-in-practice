7d319bdb1056e8bdb969251cd9ae9246c9efdbb7
Daniel Lima <fr.daniel.lima@gmail.com>
2019-07-12 15:36:49 -0300

    Tratamento de erros em Testes

--- src/map/map.js
+++ src/map/map.js
@@ -1,6 +1,12 @@
 "use strict";
 
-function map(arr = [], func = item => item) {
+function map(arr, func) {
+  if (!Array.isArray(arr))
+    throw new TypeError("The first parameter must be an array");
+
+  if (typeof func !== "function")
+    throw new TypeError("The second parameter must be a function");
+
   const newArray = [];
   for (let i = 0; i < arr.length; i++) newArray.push(func(arr[i], i, arr));
   return newArray;
